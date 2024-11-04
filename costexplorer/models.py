37746d034f9292814a9fb573dbc8a19573452d28
Rajasekhar <rajasekharchittamsetti@gmail.com>
2024-11-04 14:33:57 +0530

    initial commit

---
+++ costexplorer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Customers(models.Model):
+    customer_name = models.CharField(max_length=255)
+    aws_user_id = models.CharField(max_length=25)
+    aws_account = models.IntegerField()
+    aws_user_arn = models.CharField(max_length=50)
+    profile_pic = models.ImageField()
+
+    
+    
